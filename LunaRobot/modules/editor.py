fdd6b92b5cf00d98bf8caa0667ff328b4c69810d
dark phoenix <79581828+darkphoenix2601@users.noreply.github.com>
2021-10-28 12:23:50 +0530

    Update editor.py

--- LunaRobot/modules/editor.py
+++ LunaRobot/modules/editor.py
@@ -453,7 +453,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
 __mod_name__ = "Editor📷"
 __help__ = """
 <b> IMAGE EDITOR </b>
-LivvyBot have some advanced image editing tools inbuilt
+Akshi have some advanced image editing tools inbuilt
 Bright, Circle, RemBG, Blur, Border, Flip, Glitch, Sticker maker and more
 
 - /edit [reply to image]: Open the image editor
